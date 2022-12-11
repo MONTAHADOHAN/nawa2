87017c22a2a077117280bd0e25e26768ae045231
MONTAHADOHAN <77169330+MONTAHADOHAN@users.noreply.github.com>
2022-12-11 10:25:36 +0300

    Update jsindex.js

--- js/jsindex.js
+++ js/jsindex.js
@@ -1,4 +1,3 @@
-
 let nums = document.querySelectorAll(".nums .num");
 let section = document.querySelector(".three");
 let started = false ; // function started? no
@@ -6,8 +5,7 @@ let started = false ; // function started? no
 
 window.onscroll = function (){
     //console.log(window.scrollY , section.offsetTop)
-    if(window.scrollY >= section.offsetTop - 500){
-        
+    if(window.scrollY >= section.offsetTop ){
         if(!started) {
             nums.forEach((num) => startCount(num));
         }
@@ -15,18 +13,14 @@ window.onscroll = function (){
     }
 };
 
-function startCount(el){
+function startCount(el) {
     let goal = el.dataset.goal;
     let count = setInterval(() => {
-        let n = Number(el.innerText);
-        n++;
-        el.innerText = n;
-        console.log(el.textcontent, n)
-        if(n == goal){
-            clearInterval(count)
-        }
-    }, 2000 / goal);
-}
-
+      el.textContent++;
+      if (el.textContent == goal) {
+        clearInterval(count);
+      }
+    }, 10);
+  }
 
 //email button------------------------------------------
